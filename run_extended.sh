79c2c385b71fdae37ba9e7de7c7a1d2df2781b0f
Damith Premasiri <damithpremasiri@gmail.com>
2025-04-15 14:21:43 +0100

    lr adjust

--- run_extended.sh
+++ run_extended.sh
@@ -2,11 +2,11 @@
 
 python -m experiments.readability_assessement_V2 --model_name FacebookAI/xlm-roberta-base --model_type xlmroberta --num_train_epochs 4 --run_mode word_file_cat --n_fold 3 --append_column word_file_cat --lr 0.00001
 python -m experiments.readability_assessement_V2 --model_name FacebookAI/xlm-roberta-base --model_type xlmroberta --num_train_epochs 4 --run_mode word_file --n_fold 3 --append_column word_file --lr 0.00001
-python -m experiments.readability_assessement_V2 --model_name google-bert/bert-base-multilingual-cased --model_type bert --num_train_epochs 4 --run_mode word_file_cat --n_fold 3 --append_column word_file_cat --lr 0.00001
-python -m experiments.readability_assessement_V2 --model_name google-bert/bert-base-multilingual-cased --model_type bert --num_train_epochs 4 --run_mode word_file --n_fold 3 --append_column word_file --lr 0.00001
-python -m experiments.readability_assessement_V2 --model_name aubmindlab/araelectra-base-discriminator --model_type electra --num_train_epochs 4 --run_mode word_file_cat --n_fold 3 --append_column word_file_cat --lr 0.00001
-python -m experiments.readability_assessement_V2 --model_name aubmindlab/araelectra-base-discriminator --model_type electra --num_train_epochs 4 --run_mode word_file --n_fold 3 --append_column word_file --lr 0.00001
-python -m experiments.readability_assessement_V2 --model_name aubmindlab/bert-base-arabertv2--model_type bert --num_train_epochs 4 --run_mode word_file_cat --n_fold 3 --append_column word_file_cat --lr 0.00001
-python -m experiments.readability_assessement_V2 --model_name aubmindlab/bert-base-arabertv2 --model_type bert --num_train_epochs 4 --run_mode word_file --n_fold 3 --append_column word_file --lr 0.00001
-python -m experiments.readability_assessement_V2 --model_name CAMeL-Lab/bert-base-arabic-camelbert-mix--model_type bert --num_train_epochs 4 --run_mode word_file_cat --n_fold 3 --append_column word_file_cat --lr 0.00001
-python -m experiments.readability_assessement_V2 --model_name CAMeL-Lab/bert-base-arabic-camelbert-mix --model_type bert --num_train_epochs 4 --run_mode word_file --n_fold 3 --append_column word_file --lr 0.00001
+python -m experiments.readability_assessement_V2 --model_name google-bert/bert-base-multilingual-cased --model_type bert --num_train_epochs 4 --run_mode word_file_cat --n_fold 3 --append_column word_file_cat --lr 0.000005
+python -m experiments.readability_assessement_V2 --model_name google-bert/bert-base-multilingual-cased --model_type bert --num_train_epochs 4 --run_mode word_file --n_fold 3 --append_column word_file --lr 0.000005
+python -m experiments.readability_assessement_V2 --model_name aubmindlab/araelectra-base-discriminator --model_type electra --num_train_epochs 4 --run_mode word_file_cat --n_fold 3 --append_column word_file_cat --lr 0.000005
+python -m experiments.readability_assessement_V2 --model_name aubmindlab/araelectra-base-discriminator --model_type electra --num_train_epochs 4 --run_mode word_file --n_fold 3 --append_column word_file --lr 0.000005
+python -m experiments.readability_assessement_V2 --model_name aubmindlab/bert-base-arabertv2--model_type bert --num_train_epochs 4 --run_mode word_file_cat --n_fold 3 --append_column word_file_cat --lr 0.000005
+python -m experiments.readability_assessement_V2 --model_name aubmindlab/bert-base-arabertv2 --model_type bert --num_train_epochs 4 --run_mode word_file --n_fold 3 --append_column word_file --lr 0.000005
+python -m experiments.readability_assessement_V2 --model_name CAMeL-Lab/bert-base-arabic-camelbert-mix--model_type bert --num_train_epochs 4 --run_mode word_file_cat --n_fold 3 --append_column word_file_cat --lr 0.000005
+python -m experiments.readability_assessement_V2 --model_name CAMeL-Lab/bert-base-arabic-camelbert-mix --model_type bert --num_train_epochs 4 --run_mode word_file --n_fold 3 --append_column word_file --lr 0.000005
